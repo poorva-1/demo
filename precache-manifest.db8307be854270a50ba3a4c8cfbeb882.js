c995c2dce5e389a9bed7e6ef36fc28bd70d46582
poorva-1 <poorvamohoni@gmail.com>
2019-08-23 15:12:22 +0530

    Updates

---
+++ precache-manifest.db8307be854270a50ba3a4c8cfbeb882.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa47d2aea366088d0453e43e02ae5893",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "9dc4c40b36bc15b904d4",
+    "url": "/demo/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0c6a5a14525c7d172f14",
+    "url": "/demo/static/css/main.f525f5e8.chunk.css"
+  },
+  {
+    "revision": "9dc4c40b36bc15b904d4",
+    "url": "/demo/static/js/2.a1c23728.chunk.js"
+  },
+  {
+    "revision": "0c6a5a14525c7d172f14",
+    "url": "/demo/static/js/main.ada05245.chunk.js"
+  },
+  {
+    "revision": "2f23047b078009b94f5f",
+    "url": "/demo/static/js/runtime~main.96f7b30f.js"
+  },
+  {
+    "revision": "b2110b2139be7265c389bcb1c0736da3",
+    "url": "/demo/static/media/application_development.b2110b21.png"
+  },
+  {
+    "revision": "8e515c26d5f2d1841b123811005a181c",
+    "url": "/demo/static/media/digital.8e515c26.jpg"
+  },
+  {
+    "revision": "9f7f62750b9a2b46ede3ba0a4699fb31",
+    "url": "/demo/static/media/logo.9f7f6275.png"
+  },
+  {
+    "revision": "d67c1cca0889a9b78d1c549733219b2a",
+    "url": "/demo/static/media/w1.d67c1cca.jpg"
+  },
+  {
+    "revision": "f29ed408b4fe986f6c279fc7cfca230b",
+    "url": "/demo/static/media/w2.f29ed408.jpg"
+  },
+  {
+    "revision": "330cf43224dd89ce58640bf250d10904",
+    "url": "/demo/static/media/w3.330cf432.jpg"
+  },
+  {
+    "revision": "84825c5704118d4e22dfc47b9cea5733",
+    "url": "/demo/static/media/w4.84825c57.jpg"
+  },
+  {
+    "revision": "c5989fa8f5f1c77d7894278759f05071",
+    "url": "/demo/static/media/w5.c5989fa8.jpg"
+  },
+  {
+    "revision": "4af81a156398427bb723613c7f2dcd1c",
+    "url": "/demo/static/media/w6.4af81a15.png"
+  },
+  {
+    "revision": "7d516862f797757862805fd132ca1e6b",
+    "url": "/demo/static/media/w7.7d516862.jpg"
+  },
+  {
+    "revision": "b90021d6d0f86bec7dd4d91e3c12930e",
+    "url": "/demo/static/media/w8.b90021d6.png"
+  },
+  {
+    "revision": "264d8fb4ff3d916de76d2c03046ad500",
+    "url": "/demo/static/media/w9.264d8fb4.png"
+  },
+  {
+    "revision": "c1cf094ab09ec79959dd305b3d4a7beb",
+    "url": "/demo/static/media/web_development.c1cf094a.png"
+  },
+  {
+    "revision": "bc2f64ff230cbf4b8f6093c574985022",
+    "url": "/demo/static/media/website-development.bc2f64ff.jpg"
+  }
+]);
